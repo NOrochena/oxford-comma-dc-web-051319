74ffda00ceb3ec97822b38371e164d1aed81a981
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 06:02:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count > 2
+    last_ele = array.pop
+    str = array.join(", ")
+    str += ", and #{last_ele}"
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    array.join("")
+  end
 end
